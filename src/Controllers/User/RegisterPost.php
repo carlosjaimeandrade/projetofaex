1de5380fc0129e2af6397f98238386bb0f30362f
carlos <jaime_andrek@hotmail.com>
2024-08-29 21:51:02 -0300

    aula dia 29-08

---
+++ src/Controllers/User/RegisterPost.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Carlos\Scheduler\Controllers\User;
+
+class RegisterPost
+{
+    public function execute($data)
+    {
+        var_dump($data);
+    }
+}
---
+++ src/Models/Users/Users.php
@@ -0,0 +1,10 @@
+<?php
+namespace Carlos\Scheduler\Models\Users;
+
+use Carlos\Scheduler\Models\AbstractModel;
+
+class Users extends AbstractModel
+{
+    public $table = 'users';
+
+}
