380e5ebd3469790e1ac9c6c1a3afe37afaa2cfa3
carlos <jaime_andrek@hotmail.com>
2024-09-05 21:50:12 -0300

    Aula 05/09

--- index.php
+++ index.php
@@ -3,6 +3,18 @@
 require_once './vendor/autoload.php';
 
 use Carlos\Scheduler\Routers\Loader;
+// use Carlos\Scheduler\Models\Users\Users;
+
+// $user = new Users();
+
+// $data = [
+//     'name' => "",
+//     'email' => "carlo@example.com",
+//     'phone' => "35998309568",
+//     'password' => password_hash('12236565', PASSWORD_DEFAULT),
+// ];
+
+// $user->create($data);
 
 $loader = new Loader();
 $loader->execute();
--- src/Controllers/User/RegisterPost.php
+++ src/Controllers/User/RegisterPost.php
@@ -2,10 +2,33 @@
 
 namespace Carlos\Scheduler\Controllers\User;
 
+use Carlos\Scheduler\Models\Users\Users;
+use Carlos\Scheduler\Controllers\User\Services\Register\Validate;
+
 class RegisterPost
 {
+    protected Users $users;
+
+    protected Validate $validate;
+
+    public function __construct()
+    {
+        $this->users = new Users();
+        $this->validate = new Validate();
+    }
+
     public function execute($data)
     {
-        var_dump($data);
+        if (!$this->validate->execute($data)){
+            header('location: /register');
+            return;
+        }
+
+        $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+
+        $this->users->create($data);
+
+        header('location: /login');
+        return;
     }
 }
