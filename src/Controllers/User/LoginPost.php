dec0a9535d1321c6d3e6b910b7d39dabae39f145
carlos <jaime_andrek@hotmail.com>
2024-10-17 21:07:23 -0300

    modificação no login post

--- src/Controllers/User/LoginPost.php
+++ src/Controllers/User/LoginPost.php
@@ -48,7 +48,7 @@ class LoginPost
             return;
         }
         
-        if(!password_verify($data['password'], $user->password)) {
+        if(!password_verify($data['password'], $user['password'])) {
             $this->message->setMessageError("Usuario ou senha invalidos");
             header('location: /login');
             return;
